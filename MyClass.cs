d2bf9af8aec1634a9523ddc8b2e2ce154425ab99
ira47 <1196951630@qq.com>
2020-03-02 10:50:05 +0800

    添加了一个新类

---
+++ MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _8003_first_dotnet
+{
+    public class MyClass
+    {
+        public string ReturnMessage()
+        {
+            return "Happy coding!";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace _8003_first_dotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var c1 = new MyClass();
+            Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
         }
     }
 }

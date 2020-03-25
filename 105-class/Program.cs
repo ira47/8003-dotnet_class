56bb453366748252c88a93ee02cfa0a816d41489
ira47 <1196951630@qq.com>
2020-03-25 14:27:48 +0800

    实例构造器+属性

--- 105-class/Program.cs
+++ 105-class/Program.cs
@@ -10,12 +10,33 @@ namespace _105_class
         const Int32 _const = -1;
         readonly Int32 _read_only = 2;
         static Int32 _static = 3;
-        static Int32 _public;
-
+        public Int32 _public;
+        // 类构造器
         static SomeType()
         {
             Console.WriteLine("Hello SomeType!");
 
+        }
+        // 实例构造器
+        public SomeType()
+        {
+            Console.WriteLine("Hello New Object Sometype!");
+        }
+        public SomeType(Int32 v)
+        {
+            Console.WriteLine("Hello New With Value Sometype!");
+            _public = v;
+        }
+        public Int32 SomeProp
+        {
+            get
+            {
+                return _public;
+            }
+            set
+            {
+                _public = value;
+            }
         }
     }
     class Program
@@ -24,7 +45,8 @@ namespace _105_class
         {
             Console.WriteLine("Hello World!");
             SomeType o = new SomeType();
-            SomeType o2 = new SomeType();
+            o.SomeProp = 2020;
+            Console.WriteLine("SomeProp:{0}", o._public);
         }
     }
 }

97a62428bf7c8b58e88e0e0f387fd9fcc9394bb6
ira47 <1196951630@qq.com>
2020-03-25 14:13:57 +0800

    105-类和默认构造函数

---
+++ 105-class/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _105_class
+{
+    class SomeType
+    {
+        class SomeNestedType
+        { }
+
+        const Int32 _const = -1;
+        readonly Int32 _read_only = 2;
+        static Int32 _static = 3;
+        static Int32 _public;
+
+        static SomeType()
+        {
+            Console.WriteLine("Hello SomeType!");
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SomeType o = new SomeType();
+            SomeType o2 = new SomeType();
+        }
+    }
+}

3facd1f80a67386a6a5f9d7b1bae046a99a7a385
ira47 <1196951630@qq.com>
2020-05-18 23:08:10 +0800

    113 task

---
+++ 113-task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace task
+{
+    class task 
+    {
+        public object object;
+        public Thread thread;
+        public task(ParameterizedThreadStart starter, object _object)
+        {
+            thread = new Thread(starter);
+            object = _object;
+        }
+        public void wait()   
+        {
+            thread.Join();
+        }
+        public void start() 
+        {
+            thread.Start(object);
+        }
+    }
+    class test
+    {
+        static void output(object object)
+        {
+            Console.WriteLine("{0}", object);
+        }
+        static void Main(string[] args)
+        {
+            task my_thread = new task(output, 200); 
+            my_thread.start();
+            my_thread.wait();
+        }
+    }
+}

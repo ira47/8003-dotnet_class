0066839e9af86403233bd66e272cb4fd9ed16063
ira47 <1196951630@qq.com>
2020-07-09 21:32:11 +0800

    NotesLibrary 多线程演示

---
+++ 200-final_program/NotesLibrary/MultiThread/MultiThread.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MultiThread
+{
+    public class MultiLearning
+    {
+        public List<string> message = new List<string>();
+        public Object locker = new Object();
+
+        public void StartLearning(List<string> BookNames)
+        {
+            lock (locker)
+                message.Add("开始执行多线程任务。。");
+            ThreadPool.SetMaxThreads(2, 2);
+            foreach (string bookName in BookNames)
+            {
+                ThreadPool.QueueUserWorkItem(LearningTask, bookName);
+            }
+        }
+
+        public void LearningTask(object BookName)
+        {
+            string bookName = BookName.ToString();
+            Random random = new Random();
+            int n = random.Next(500, 1000);
+            for (int count = 1; count <= 10; count++)
+            {
+                Thread.Sleep(n);
+                string m = DateTime.Now.ToString() + " 书籍 " + bookName
+                        + " 阅读到第 " + count.ToString() + " 页";
+                lock (locker)
+                    message.Add(m);
+            }
+        }
+        public List<string> GetMessages()
+        {
+            lock (locker)
+                return message;
+        }
+    }
+}
---
+++ 200-final_program/NotesLibrary/NotesLibrary/Controllers/ThreadController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultiThread;
+using NotesLibrary.Models;
+using ViewModel;
+
+namespace NotesLibrary.Controllers
+{
+    public class ThreadController : Controller
+    {
+        public static MultiLearning multiLearning = new MultiLearning();
+        public ActionResult Index()
+        {
+            var messages = multiLearning.GetMessages();
+            return View(new ThreadViewModel { Messages = messages});
+        }
+        public ActionResult Start()
+        {
+            using (LibraryDBContext db = new LibraryDBContext())
+            {
+                List<string> bookNames = new List<string>();
+                var books = db.BookInfoes.ToList();
+                int count = 0;
+                foreach (var book in books)
+                    bookNames.Add((++count).ToString() + " " + book.Name);
+                multiLearning.StartLearning(bookNames);
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
